1aa7477a4c05de4ad6fffe1f170a1185f18fdfcd
Diego Iván García Escobedo <digaresc@gmail.com>
2017-09-23 09:34:31 -0700

    Estructura basica de Twitter Login con Firebase

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD9zV7WHzRlT4PTElHVGaRy0P8cd_A1bvQ",
+    authDomain: "revisamigrieta.firebaseapp.com",
+    databaseURL: "https://revisamigrieta.firebaseio.com",
+    projectId: "revisamigrieta",
+    storageBucket: "revisamigrieta.appspot.com",
+    messagingSenderId: "427953002306"
+};
+firebase.initializeApp(config);
