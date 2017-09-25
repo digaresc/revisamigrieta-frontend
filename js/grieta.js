083991e64f72ea5bcf517679cdbb3bcd81da7987
Diego Iván García Escobedo <digaresc@gmail.com>
2017-09-24 18:29:42 -0700

    Grieta y Grietas funcionando

--- js/grieta.js
+++ js/grieta.js
@@ -1,4 +1,4 @@
-function loadGrietas() {
+function loadGrietas(grietaId) {
 
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
@@ -10,7 +10,7 @@ function loadGrietas() {
                 $("#con-close-modal").modal();
             } else {
                 if (Object.getOwnPropertyNames(d).length > 0) {
-
+                    console.log(d);
                     grietasJsonToHTML(d);
 
                 }
@@ -20,7 +20,7 @@ function loadGrietas() {
         }
     });
 
-    xhr.open("GET", BASE_URL + "/grietas");
+    xhr.open("GET", BASE_URL + "/grietas/" + grietaId + "/revisiones");
     xhr.setRequestHeader("content-type", "application/json");
     xhr.setRequestHeader("cache-control", "no-cache");
 
@@ -58,7 +58,49 @@ for( var i=0; i < json['items'].length; i++ ){
 
 
 }
+function getAllUrlParams(url) {
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    var obj = {};
+    if (queryString) {
+
+        queryString = queryString.split('#')[0];
+        var arr = queryString.split('&');
+
+        for (var i = 0; i < arr.length; i++) {
+            var a = arr[i].split('=');
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function (v) {
+                paramNum = v.slice(1, -1);
+                return '';
+            });
+
+            var paramValue = typeof(a[1]) === 'undefined' ? true : a[1];
+            paramName = paramName.toLowerCase();
+            paramValue = paramValue.toLowerCase();
+
+            if (obj[paramName]) {
+                if (typeof obj[paramName] === 'string') {
+                    obj[paramName] = [obj[paramName]];
+                }
+                if (typeof paramNum === 'undefined') {
+                    obj[paramName].push(paramValue);
+                }
+                else {
+                    obj[paramName][paramNum] = paramValue;
+                }
+            }
+            else {
+                obj[paramName] = paramValue;
+            }
+        }
+    }
+    return obj;
+}
+
+
+
 
 window.onload = function () {
-    loadGrietas();
+    var grietaId = decodeURIComponent(getAllUrlParams().id);
+    loadGrietas(grietaId);
 }
