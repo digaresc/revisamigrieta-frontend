8cba59e1507c88b569b26c7db7eefd2f7597b09a
Claudio M <cm@paybook.com>
2017-09-25 23:56:11 -0500

    Paginación de grietas con angular

---
+++ js/app.js
@@ -0,0 +1,4 @@
+var revisaMiGrieta = angular.module('revisaMiGrieta', ['angularUtils.directives.dirPagination']);
+
+revisaMiGrieta.controller('Grietas', Grietas);
+revisaMiGrieta.controller('Pagination', Pagination);
